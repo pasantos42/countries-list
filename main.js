744caa7f811f64dfe10b5ec3de25e5896016fcb0
Patricia <patricia.sm134@gmail.com>
2023-04-25 00:40:12 +0100

    refactor: improve code organization and readability

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 import './style.scss'
 
+let maxResults = 15;
+let transitionTime = 100;
+
 const btn = document.querySelector('button');
 const input = document.querySelector('input');
 const grid = document.querySelector('.countries-grid');
 const log = document.createElement('p');
-const resultsnbr = 15;
-
-grid.insertAdjacentElement('beforebegin', log);
 log.classList.add('log');
 
 btn.addEventListener('click', function(event) {
@@ -16,8 +16,8 @@ btn.addEventListener('click', function(event) {
 
 input.addEventListener('keydown', function(event) {
   if (event.keyCode == 13) {
-    console.log("Object: ", event.target);
-    console.log("Input: " + input.value);
+    console.log(`Object: ${event.target}`);
+    console.log(`Input: ${input.value}`);
     fetchCountry(input.value);
   }
 });
@@ -25,15 +25,15 @@ input.addEventListener('keydown', function(event) {
 async function fetchCountry(input)
 {
   try {
-    grid.innerHTML = '';
+    grid.innerHTML = ''; //clean previous search
     const response = await fetch (`https://restcountries.com/v3.1/region/${input}`);
     if (!response.ok) {
       throw new Error(response.status);
     }
     const data = await response.json();
-    log.innerText = '';
-    console.log(data[1]);
-    for (let i = 0; i <= resultsnbr; i++)
+
+    log.remove();
+    for (let i = 0; i <= maxResults; i++)
     {
       const card = document.createElement('a');
       const flag = document.createElement('img');
@@ -57,11 +57,7 @@ async function fetchCountry(input)
       population.innerText = `${data[i].population} inhabitants`;
       subregion.innerText = `Region: ${data[i].subregion}`;
       timezone.innerText = `Timezone: ${data[i].timezones[0]}`
-      if (data[i].independent == true) {
-        dependancy.innerText = 'independent';
-      } else {
-        dependancy.innerText = 'dependent';
-      }
+      data[i].independent ? dependancy.innerText = 'independent' : dependancy.innerText = 'dependent';
 
       card.classList.add('card');
       card.classList.add('card--invisible');
@@ -85,19 +81,24 @@ async function fetchCountry(input)
       details.insertAdjacentElement('beforeend', dependancy);
       details.insertAdjacentElement('beforeend', timezone);
 
+      // card-display transition
       setTimeout(() => {  
         card.classList.remove('card--invisible');
-      }, i * 100);
+      }, i * transitionTime);
 
+      // for page travelling
       card.setAttribute('href', '#');
       card.setAttribute('id', `card${i}`);
 
       card.addEventListener('click', function() {
         if (card.classList.contains('card--active')) {
-          capital.innerText = data[i].capital[0];
-          card.insertAdjacentElement('beforeend', capital);
-          capital.classList.add('card__info--secondary');
 
+          // transfering 'capital' out of 'details'
+          capital.innerText = data[i].capital[0];
+          capital.classList.add('card__info--secondary');
+          card.insertAdjacentElement('beforeend', capital);
+
+          // reseting tag-display transition
           let tags = document.querySelectorAll('.card--active p');
           for (let j = 0; j < tags.length; j++) {
               tags[j].classList.remove('tag--visible');
@@ -110,20 +111,22 @@ async function fetchCountry(input)
             card.removeAttribute('href', `#card${i - 3}`);
             card.setAttribute('href', '#');
           }, 500);
+
         } else {
-
           card.classList.add('card--active');
-          capital.classList.remove('card__info--secondary');
-          card.insertAdjacentElement('beforeend', details);
-          capital.innerText = `Capital: ${data[i].capital[0]}`;
-          details.insertAdjacentElement('afterbegin', capital);
 
+          // transfering 'capital' to 'details'
+          capital.innerText = `Capital: ${data[i].capital[0]}`;
+          capital.classList.remove('card__info--secondary');
+          details.insertAdjacentElement('afterbegin', capital);
+          card.insertAdjacentElement('beforeend', details);
+
+          // tag-display transition
           let tags = document.querySelectorAll('.card--active p');
-          console.log("🚀 ~ file: main.js:121 ~ card.addEventListener ~ tags:", tags)
           for (let j = 0; j < tags.length; j++) {
             setTimeout(() => {
               tags[j].classList.add('tag--visible');
-            }, j * 100);
+            }, j * transitionTime);
           }
       
           setTimeout(() => {
@@ -134,6 +137,7 @@ async function fetchCountry(input)
       })
     }
   } catch (error) {
+    grid.insertAdjacentElement('beforebegin', log);
     log.innerText = 'unable to fetch countries';
   }
 }
